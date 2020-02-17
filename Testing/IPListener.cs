9cf700ffe92453aa6228b96c24fdd0dd543300b3
Zach Richling <zrichling@amberpharmacy.com>
2020-02-17 14:54:09 -0600

    Add project files.

---
+++ Testing/IPListener.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Timers;
+
+namespace Testing {
+    public class IPListener {
+
+        private readonly Timer _timer;
+        private string currentIp = new WebClient().DownloadString("https://api.ipify.org");
+        public IPListener(int repeat) {
+            _timer = new Timer(repeat) { AutoReset = true};
+            _timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+        }
+
+        public void OnTimedEvent(object source, ElapsedEventArgs e) {
+            executeInstruction(ingestInstruction());
+            ensureIPCorrectness();
+        }
+
+        private void ensureIPCorrectness() {
+            string externalip = "";
+            try {
+                externalip = new WebClient().DownloadString("https://api.ipify.org");
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+            if (externalip != currentIp) {
+                Console.WriteLine("IP Changed from {0} to {1}.", currentIp, externalip);
+                currentIp = externalip;
+                Console.WriteLine("Current IP: " + currentIp);
+            } else {
+                Console.WriteLine("No change...");
+            }
+        }
+
+        private string ingestInstruction() {
+            return "";
+        }
+
+        private void executeInstruction(String instruction) {
+            if (instruction.Equals("")) {
+                Console.WriteLine("No instruction received.");
+            } else {
+                Console.WriteLine("Instruction Received");
+            }
+        }
+        public void Start() {
+            Console.WriteLine("Current IP: " + currentIp);
+            Console.WriteLine("Current Machine Name: " + Environment.MachineName);
+            _timer.Start();
+        }
+
+        public void Stop() {
+            _timer.Stop();
+        }
+
+        public string getCurrentIP() {
+            return currentIp;
+        }
+
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Timers;
+using Topshelf;
+
+namespace Testing {
+    class Program {
+        public static void Main() {
+            var exitCode = HostFactory.Run(x => { 
+                x.Service<IPListener>(s => {
+                    s.ConstructUsing(ipListener => new IPListener(5000));
+                    s.WhenStarted(ipListener => ipListener.Start());
+                    s.WhenStopped(ipListener => ipListener.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("KutakRockTicketHelper");
+                x.SetDisplayName("Kutak Rock Ticket Helper");
+                x.SetDescription("This Service relay's machine information to the Kutak Rock Ticketing System website.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
