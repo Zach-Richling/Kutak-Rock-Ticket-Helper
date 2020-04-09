a5897c1b0bc8d513c2afc3be79e73c8174d24db4
Xavier McCaig <xaviermccaig@gmail.com>
2020-04-09 11:51:27 -0500

    Further fleshed out MachineInfo information gathering

--- Client Side Helper/Ticket Helper.cs
+++ Client Side Helper/Ticket Helper.cs
@@ -3,31 +3,75 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Timers;
+using System.Management;
+using NetworkInterface;
 using System.Management.Automation;
+using System.Reflection.PortableExecutable;
+using Microsoft.VisualBasic.Devices;
 
 namespace KutakRock {
     public class Ticket_Helper {
 
         public class MachineInfo
         {
-            public string id = "0";
-            public string ip = "";
-            public string machineName = "";
-            public string OSName = "";
-            public string OSVersion = "";
-            public string userName = "";
+            public string id = "0"; //needs some sort of integration with DB @ Zach
+            public string ip = "";  
+            public string machineName = Environment.MachineName;
+            public string OSName = System.Environment.OSVersion.Platform;
+            public string OSVersion = System.Environment.OSVersion;
+            public string userName = Environment.UserName;
             public string manufacturer = "";
-            public string model = "";
-            public string BIOSNumber = "";
+            public string name = "";
+            public string model = ""; // Not Done
+            public string serial = "";
+            public string BIOSNumber = ""; //Number vs version?
             public string BIOSVersion = "";
-            public string macAddress = "";
-            public string ramAmount = "";
+            public string macAddress = (
+                                            from nic in NetworkInterface.GetAllNetworkInterfaces()
+                                            where nic.OperationalStatus == OperationalStatus.Up
+                                            select nic.GetPhysicalAddress().ToString()
+                                        ).FirstOrDefault();
+            public string ramAmount = Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory.ToString();
 
         }
+
         private readonly Timer _timer;
         private string currentIp = new WebClient().DownloadString("https://api.ipify.org");
         WebRequest webRequest = WebRequest.Create(new Uri("www.google.com"));
         
+        //Used to gatehr information about the BIOS
+        public void GetBiosInformation()
+        {
+            string Serial = "";
+            string BiosVer = "";
+            string ManuFac = "";
+            string Name = "";
+
+            try
+            {
+                ManagementObjectSearcher MOS = new ManagementObjectSearcher("SELECT SerialNumber, BIOSVersion, Manufacturer, Name FROM Win32_BIOS");
+                ManagementObjectCollection collection = MOS.Get();
+                foreach (ManagementObject obj in collection)
+                {
+                    Serial = (string)obj["SerialNumber"];
+                    BiosVer = (string)obj["BIOSVersion"];
+                    ManuFac = (string)obj["Manufacturer"];
+                    Name = (string)obj["Name"];
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            MachineInfo.manufacturer = ManuFac;
+            MachineInfo.BIOSVersion = BiosVer;
+            MachineInfo.name = Name;
+            MachineInfo.serial = Serial;
+        }
+
+     
+
         public Ticket_Helper(int repeat) {
             _timer = new Timer(repeat) { AutoReset = true};
             _timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
