bd8549feb033a8dd9fa6c025f8ae4634da44abec
Xavier McCaig <xaviermccaig@gmail.com>
2020-04-15 10:12:45 -0500

    Further WebRequest work

--- Client Side Helper/Ticket Helper.cs
+++ Client Side Helper/Ticket Helper.cs
@@ -8,6 +8,8 @@ using NetworkInterface;
 using System.Management.Automation;
 using System.Reflection.PortableExecutable;
 using Microsoft.VisualBasic.Devices;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
 
 namespace KutakRock {
     public class Ticket_Helper {
@@ -31,68 +33,86 @@ namespace KutakRock {
 
         }
 
+        WebRequest machineName = WebRequest.Create("http://kutakrockwcf.azurewebsites.net/Database_Service.svc/GetMachineInfo/MACHINENAME");
+
+        HttpWebRequest httpMachineName = (HttpWebRequest)machineName;                                       /////
+        httpMachineName.PreAuthenticate = true;                                                             ///// Zach pls
+        httpMachineName.Headers.Add("Authorization", "Basic" + "aJt5o3jQPOnkNuycYiuArPBPpPjwEHaR");         /////
+
+        string test = Environment.MachineName;
+
+        string MachConf = httpMachineName.ToString();
+
+        if(MachConf=){ //If MACHINENAME endpoint returns 0, indicating the machine does not exist in the DB...
+
+            MachineInfo MachInf     = new MachineInfo();
+
+            MachInf id              = "0"; //needs some sort of integration with DB @ Zach
+            MachInf ip              = "";  
+            MachInf machineName     = Environment.MachineName;
+            MachInf OSName          = Environment.OSVersion.Platform;
+            MachInf OSVersion       = Environment.OSVersion;
+            MachInf userName        = Environment.UserName;
+            MachInf manufacturer    = "";
+            MachInf name            = "";
+            MachInf model           = ""; 
+            MachInf serial          = "";
+            MachInf BIOSNumber      = ""; //Number vs version?
+            MachInf BIOSVersion     = "";
+            MachInf macAddress      = (from nic in NetworkInterface.GetAllNetworkInterfaces() 
+                                        where nic.OperationalStatus == OperationalStatus.Up 
+                                        select nic.GetPhysicalAddress().ToString()
+                                        ).FirstOrDefault();
+            MachInf ramAmount       = Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory.ToString();
+        
+            public void getModel()
+            {
+                PowerShell ModelShell   = PowerShell.Create("get-process").AddCommand("$ModelVar | Get-CimInstance -ClassName Win32_ComputerSystem"); ////////////////////////////////////////$ModelVar?
+                ModelShell.invoke();
+                MachInf model           = ModelShell.ToString();
+            }
+
+            //Used to gatehr information about the BIOS
+            public void GetBiosInformation()
+            {
+                string Serial   = "";
+                string BiosVer  = "";
+                string ManuFac  = "";
+                string Name     = "";
+
+                try
+                {
+                    ManagementObjectSearcher MOS = new ManagementObjectSearcher("SELECT SerialNumber, BIOSVersion, Manufacturer, Name FROM Win32_BIOS");
+                    ManagementObjectCollection collection = MOS.Get();
+                    foreach (ManagementObject obj in collection)
+                    {
+                        Serial  = (string)obj["SerialNumber"];
+                        BiosVer = (string)obj["BIOSVersion"];
+                        ManuFac = (string)obj["Manufacturer"];
+                        Name    = (string)obj["Name"];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                MachInf manufacturer = ManuFac;
+                MachInf BIOSVersion  = BiosVer;
+                MachInf name         = Name;
+                MachInf serial       = Serial;
+            }
+
+        }
+
+
+        WebRequest machineName = WebRequest.Create("http://kutakrockwcf.azurewebsites.net/Database_Service.svc/AddComputer/EXTERNAL_IP/MACHINE_NAME"); //push to this endpoint once info has been gathered
 
         private readonly Timer _timer;
         private string currentIp    = new WebClient().DownloadString("https://api.ipify.org");
         WebRequest webRequest       = WebRequest.Create(new Uri("www.google.com"));
         
-        MachineInfo MachInf         = new MachineInfo();
-
-        MachInf id              = "0"; //needs some sort of integration with DB @ Zach
-        MachInf ip              = "";  
-        MachInf machineName     = Environment.MachineName;
-        MachInf OSName          = Environment.OSVersion.Platform;
-        MachInf OSVersion       = Environment.OSVersion;
-        MachInf userName        = Environment.UserName;
-        MachInf manufacturer    = "";
-        MachInf name            = "";
-        MachInf model           = ""; 
-        MachInf serial          = "";
-        MachInf BIOSNumber      = ""; //Number vs version?
-        MachInf BIOSVersion     = "";
-        MachInf macAddress      = (from nic in NetworkInterface.GetAllNetworkInterfaces() 
-                                    where nic.OperationalStatus == OperationalStatus.Up 
-                                    select nic.GetPhysicalAddress().ToString()
-                                    ).FirstOrDefault();
-        MachInf ramAmount       = Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory.ToString();
         
-        public void getModel()
-        {
-            PowerShell ModelShell   = PowerShell.Create("get-process").AddCommand("$ModelVar | Get-CimInstance -ClassName Win32_ComputerSystem");
-            ModelShell.invoke();
-            MachInf model           = ModelShell.ToString();
-        }
-
-        //Used to gatehr information about the BIOS
-        public void GetBiosInformation()
-        {
-            string Serial   = "";
-            string BiosVer  = "";
-            string ManuFac  = "";
-            string Name     = "";
-
-            try
-            {
-                ManagementObjectSearcher MOS = new ManagementObjectSearcher("SELECT SerialNumber, BIOSVersion, Manufacturer, Name FROM Win32_BIOS");
-                ManagementObjectCollection collection = MOS.Get();
-                foreach (ManagementObject obj in collection)
-                {
-                    Serial  = (string)obj["SerialNumber"];
-                    BiosVer = (string)obj["BIOSVersion"];
-                    ManuFac = (string)obj["Manufacturer"];
-                    Name    = (string)obj["Name"];
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            MachInf manufacturer = ManuFac;
-            MachInf BIOSVersion  = BiosVer;
-            MachInf name         = Name;
-            MachInf serial       = Serial;
-        }
 
         public Ticket_Helper(int repeat) {
             _timer = new Timer(repeat) { AutoReset = true};
@@ -141,7 +161,7 @@ namespace KutakRock {
 
         public void pushMachineInfo() {
             string MockJson = JsonConvert.SerializeObject<MachineInfo>(MachInf);
-
+            WebRequest machineName = WebRequest.Create("http://kutakrockwcf.azurewebsites.net/Database_Service.svc/AddMachineInfo/MACHINE_NAME?machineInfo=SERIALIZED_STRING_OF_MACHINE_INFO_OBJECT_IN_JSON_FORMAT"); //push the jsonMachInf to this endpoint
         }
 
 
